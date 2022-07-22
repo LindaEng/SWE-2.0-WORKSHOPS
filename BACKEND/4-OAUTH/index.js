6757ad5606f1804e27f0b0d9065ddb1abc851056
= <90lineng@gmail.com>
2022-07-22 12:40:11 -0400

    backend oauth added

--- BACKEND/4-OAUTH/index.js
+++ BACKEND/4-OAUTH/index.js
@@ -8,7 +8,8 @@ const morgan = require('morgan');
 // create express app
 const app = express();
 // pull PORT from environment variables
-const { PORT = 3000 } = process.env;
+// const { PORT = 3000 } = process.env;
+
 
 // middleware
 // enable CORS
@@ -21,23 +22,45 @@ app.use(express.urlencoded({extended:true}));
 
 /* ************ START OIDC CODE ************ */
 
-//Set up your Auth0 account -> create new application -> get client ID + config object
+//Set up your Auth0 account -> create new application -> get client ID + config object [x]
 
-// env vars from process.env
+//importing env vars from .env file
+const {
+  AUTH0_SECRET,
+  AUTH0_BASEURL,
+  AUTH0_CLIENTID,
+  AUTH0_ISSUER_BASE_URL,
+  PORT
+} = process.env
 
 
 // define the config object 
-
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: AUTH0_SECRET,
+  baseURL: AUTH0_BASEURL,
+  clientID: AUTH0_CLIENTID,
+  issuerBaseURL: AUTH0_ISSUER_BASE_URL
+};
 
 //check your enviornmental variables with console logs
+console.log('AUTH0_SECRET', AUTH0_SECRET)
+console.log('AUTH0_BASEURL', AUTH0_BASEURL)
+console.log('AUTH0_CLIENTID', AUTH0_CLIENTID)
+console.log('AUTH0_ISSUER_BASE_URL', AUTH0_ISSUER_BASE_URL)
 
   
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-
+app.use(auth(config));
 
 // create a GET / route handler that sends back Logged in or Logged out
 // req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
+//create another get route - if a user is logged in, render something, if they are not verified/logged in render something else.
 
 /* ************ END OIDC CODE ************ */
 
@@ -48,7 +71,7 @@ app.use((error, req, res, next) => {
   res.send({error: error.message, name: error.name, message: error.message});
 });
 
-app.listen(PORT, () => {
+app.listen(() => {
   console.log(`Server is up at http://localhost:${PORT}`);
 });
 
