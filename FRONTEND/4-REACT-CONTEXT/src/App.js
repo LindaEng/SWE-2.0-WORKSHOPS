27195a29de29ac4b17e15c3240552982db301d9a
= <90lineng@gmail.com>
2022-07-22 14:39:33 -0400

    react-context starter code added

---
+++ FRONTEND/4-REACT-CONTEXT/src/App.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { CountContext } from './CountProvider';
+
+function App() {
+  const { count, increment, decrement } = useContext(CountContext);
+
+  return (
+    <div className='layout'>
+      <h1>Count provider</h1>
+      <h2>The count is: {count}</h2>
+      <button
+        onClick={increment}
+        className='btn btn-primary'
+      >
+        count++
+      </button>
+      <button
+        onClick={decrement}
+        className='btn btn-primary'
+      >
+        count--
+      </button>
+    </div>
+  );
+}
+
+export { App };
---
+++ FRONTEND/4-REACT-CONTEXT/src/CountProvider.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react';
+
+const CountContext = createContext();
+
+function CountProvider({ children }) {
+  console.log('THIS IS CHILDREN ===', children)
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <CountContext.Provider value={{ count, increment, decrement }}>
+      {children}
+    </CountContext.Provider>
+  );
+}
+
+export { CountProvider, CountContext };
